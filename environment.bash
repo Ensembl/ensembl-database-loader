57de165f955a096d4f336a44bcae2691e8de5152
Andrew D Yates <ady@sanger.ac.uk>
2011-09-16 14:45:15 +0100

    Forgot to add this project

--- environment.bash
+++ environment.bash
@@ -1,2 +1,2 @@
-export PERL5LIB=$PWD/../ensembl/modules:$PWD/../ensembl-hive/modules:$PWD/../bioperl:$PERL5LIB
+export PERL5LIB=$PWD/modules:$PWD/../ensembl/modules:$PWD/../ensembl-hive/modules:$PWD/../bioperl:$PERL5LIB
 export PATH=$PWD/../ensembl-hive/scripts:$PATH
